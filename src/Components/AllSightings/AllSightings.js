886611e499065f001e70eed80c8b1bc52bd13e2b
Riley Martin <riley.willow@gmail.com>
2022-09-28 15:48:18 -0500

    Retrieve sightings data and display sightings cards

--- src/Components/AllSightings/AllSightings.js
+++ src/Components/AllSightings/AllSightings.js
@@ -1,13 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import SightingCard from '../SightingCard/SightingCard'
+import { retrieveSightings } from '../../utilities/ApiCalls'
 
 const AllSightings = () => {
   const [sightingList, setSightingList] = useState([])
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    let mounted = true
+    retrieveSightings()
+      .then(sightings => {
+        if(mounted) {
+          setSightingList(sightings)
+        }
+      })
+      .catch(error => setError('Unable to retrieve data'))
+    return () => mounted = false
+  }, [sightingList])
+
+  const allSightings = sightingList?.map(sighting => {
+    return <SightingCard />
+  })
 
   return(
     <article>
       <h2>Documented Sightings</h2>
-      <SightingCard />
+      {allSightings}
     </article>
   )
 }
--- src/utilities/ApiCalls.js
+++ src/utilities/ApiCalls.js
@@ -1,3 +1,12 @@
 const retrieveSightings = () => {
-
+  return fetch('http://localhost:3001/sightings')
+    .then(response => {
+      if(!response.ok) {
+        throw new Error('Unable to retrieve server data')
+      } else {
+        return response.json()
+      }
+    })
 }
+
+export { retrieveSightings }
