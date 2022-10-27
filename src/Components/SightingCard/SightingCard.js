33ec4293d59086d98a77064385af214d2aa0c90e
Riley Martin <riley.willow@gmail.com>
2022-10-27 15:09:27 -0500

    Create button to delete sighting

--- src/Components/SightingCard/SightingCard.js
+++ src/Components/SightingCard/SightingCard.js
@@ -1,9 +1,16 @@
+import { deleteSighting } from '../../utilities/ApiCalls'
+
 const SightingCard = ({ id, location, description }) => {
+  const deleteCard = (id) => {
+    deleteSighting(id)
+  }
+
   return(
     <section>
       <h3>Sighting {id}</h3>
       <h4>Location: {location}</h4>
       <p>{description}</p>
+      <button onClick={event => deleteSighting(id)}>delete</button>
     </section>
   )
 }
--- src/utilities/ApiCalls.js
+++ src/utilities/ApiCalls.js
@@ -1,29 +1,36 @@
-const retrieveSightings = () => {
-  return fetch('http://localhost:3001/sightings')
-    .then(response => {
-      if(!response.ok) {
-        throw new Error('Unable to retrieve server data')
-      } else {
-        return response.json()
-      }
-    })
+export const retrieveSightings = async () => {
+  const response = await fetch('http://localhost:3001/sightings')
+    const data = await errorHandler(response)
+    return data
 }
 
-const reportSighting = (sightingInfo) => {
-  return fetch('http://localhost:3001/sightings', {
+export const reportSighting = async (sightingInfo) => {
+  const response = await fetch('http://localhost:3001/sightings', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(sightingInfo)
   })
-  .then(response => {
-    if(!response.ok) {
-      throw new Error('Unable to reach server data')
-    } else {
-      return response.json()
-    }
-  })
+  const data = await errorHandler(response)
+  return data
 }
 
-export { retrieveSightings, reportSighting }
+export const deleteSighting = async (id) => {
+  const response = await fetch(`http://localhost:3001/sightings/${id}`,{
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+  const data = await errorHandler(response)
+  return data
+}
+
+export const errorHandler = (response) => {
+  if (!response.ok) {
+    throw new Error(response.message)
+  } else {
+    return response.json()
+  }
+}
